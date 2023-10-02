6dfca383c815dcdc20a498bbdc840d861b70d561
MorteX179 <70376091+MorteX179@users.noreply.github.com>
2023-10-02 20:36:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,103 +1,48 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const diceTab = document.getElementById("dice-tab");
-    const armoryTab = document.getElementById("armory-tab");
-    const diceContent = document.getElementById("dice-content");
-    const armoryContent = document.getElementById("armory-content");
+// Add the following code after the Dice Roller functionality
+const armoryRollButton = document.getElementById("armory-roll-button");
+const weaponNameInput = document.getElementById("weapon-name");
+const weaponDiceSelect = document.getElementById("weapon-dice");
+const weaponNumRollsInput = document.getElementById("weapon-num-rolls");
+const weaponBonusPointsInput = document.getElementById("weapon-bonus-points");
+const armorySaveButton = document.getElementById("armory-save-button");
+const savedWeapons = document.getElementById("saved-weapons");
+const armoryResult = document.getElementById("armory-result");
 
-    diceTab.addEventListener("click", function () {
-        diceContent.style.display = "block";
-        armoryContent.style.display = "none";
-    });
+const weapons = [];
 
-    armoryTab.addEventListener("click", function () {
-        armoryContent.style.display = "block";
-        diceContent.style.display = "none";
-    });
+armoryRollButton.addEventListener("click", function () {
+    const diceType = parseInt(weaponDiceSelect.value);
+    const numRolls = parseInt(weaponNumRollsInput.value);
+    const bonusPoints = parseInt(weaponBonusPointsInput.value);
 
-    const rollDiceButton = document.getElementById("roll-dice-button");
-    const diceTypeSelect = document.getElementById("dice-type");
-    const bonusPointsInput = document.getElementById("bonus-points");
-    const diceResult = document.getElementById("dice-result");
+    let totalResult = 0;
+    let rolledNumbers = [];
 
-    rollDiceButton.addEventListener("click", function () {
-        const diceType = parseInt(diceTypeSelect.value);
-        const bonusPoints = parseInt(bonusPointsInput.value);
-
+    for (let i = 0; i < numRolls; i++) {
         const rolledNumber = Math.max(1, Math.floor(Math.random() * diceType) + 1);
-        const totalResult = Math.max(1, rolledNumber + bonusPoints);
+        rolledNumbers.push(rolledNumber);
+        totalResult += rolledNumber;
+    }
 
-        diceResult.innerHTML = `<p>Total Result: ${totalResult}</p><p>Rolled: ${rolledNumber}</p><p>Bonus Points: ${bonusPoints}</p>`;
-    });
+    totalResult = Math.max(1, totalResult + bonusPoints);
 
-    const armoryRollButton = document.getElementById("armory-roll-button");
-    const weaponNameInput = document.getElementById("weapon-name");
-    const weaponDiceSelect = document.getElementById("weapon-dice");
-    const weaponNumRollsInput = document.getElementById("weapon-num-rolls");
-    const weaponBonusPointsInput = document.getElementById("weapon-bonus-points");
-    const armorySaveButton = document.getElementById("armory-save-button");
-    const savedWeapons = document.getElementById("saved-weapons");
-    const armoryResult = document.getElementById("armory-result");
-
-    const weapons = [];
-
-    armoryRollButton.addEventListener("click", function () {
-        const diceType = parseInt(weaponDiceSelect.value);
-        const numRolls = parseInt(weaponNumRollsInput.value);
-        const bonusPoints = parseInt(weaponBonusPointsInput.value);
-
-        let totalResult = 0;
-        let rolledNumbers = [];
-
-        for (let i = 0; i < numRolls; i++) {
-            const rolledNumber = Math.max(1, Math.floor(Math.random() * diceType) + 1);
-            rolledNumbers.push(rolledNumber);
-            totalResult += rolledNumber;
-        }
-
-        totalResult = Math.max(1, totalResult + bonusPoints);
-
-        armoryResult.innerHTML = `<p>Total Result: ${totalResult}</p><p>Rolled: ${rolledNumbers.join(", ")}</p><p>Bonus Points: ${bonusPoints}</p>`;
-    });
-
-    armorySaveButton.addEventListener("click", function () {
-        const name = weaponNameInput.value;
-        const diceType = parseInt(weaponDiceSelect.value);
-        const numRolls = parseInt(weaponNumRollsInput.value);
-        const bonusPoints = parseInt(weaponBonusPointsInput.value);
-
-        const weapon = {
-            name: name,
-            diceType: diceType,
-            numRolls: numRolls,
-            bonusPoints: bonusPoints,
-        };
-
-        weapons.push(weapon);
-
-        const weaponItem = document.createElement("div");
-        weaponItem.className = "saved-weapon";
-        weaponItem.innerHTML = `<p>${name}</p>`;
-
-        weaponItem.addEventListener("click", function () {
-            const { name, diceType, numRolls, bonusPoints } = weapon;
-            let totalResult = 0;
-            let rolledNumbers = [];
-
-            for (let i = 0; i < numRolls; i++) {
-                const rolledNumber = Math.max(1, Math.floor(Math.random() * diceType) + 1);
-                rolledNumbers.push(rolledNumber);
-                totalResult += rolledNumber;
-            }
-
-            totalResult = Math.max(1, totalResult + bonusPoints);
-
-            armoryResult.innerHTML = `<p>Total Result: ${totalResult}</p><p>Rolled: ${rolledNumbers.join(", ")}</p><p>Bonus Points: ${bonusPoints}</p>`;
-        });
-
-        savedWeapons.appendChild(weaponItem);
-
-        weaponNameInput.value = "";
-        weaponNumRollsInput.value = 1;
-        weaponBonusPointsInput.value = 0;
-    });
+    armoryResult.innerHTML = `<p>Total Result: ${totalResult}</p><p>Rolled: ${rolledNumbers.join(", ")}</p><p>Bonus Points: ${bonusPoints}</p>`;
 });
+
+armorySaveButton.addEventListener("click", function () {
+    const name = weaponNameInput.value;
+    const diceType = parseInt(weaponDiceSelect.value);
+    const numRolls = parseInt(weaponNumRollsInput.value);
+    const bonusPoints = parseInt(weaponBonusPointsInput.value);
+
+    const weapon = {
+        name: name,
+        diceType: diceType,
+        numRolls: numRolls,
+        bonusPoints: bonusPoints,
+    };
+
+    weapons.push(weapon);
+
+    const weaponItem = document.createElement("div");
+    weaponItem.className = "saved-weapon
